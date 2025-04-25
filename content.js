a55dab6342482fb21fa8bf6b391654ec4570ea86
ajoealex <ajoealex316@gmail.com>
2025-04-25 11:38:53 +0530

    modified with stricter permissions

--- content.js
+++
@@ -1,8 +0,0 @@
-// chrome.runtime.sendMessage({
-//     type: "STORAGE_DATA",
-//     url: location.href,
-//     title: document.title,
-//     localStorage: { ...localStorage },
-//     sessionStorage: { ...sessionStorage }
-//   });
-  
